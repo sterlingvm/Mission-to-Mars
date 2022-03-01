e946898680ae9a1901425f2600e4df4e59f535c3
sterlingvm <sterlingvmiller1@gmail.com>
2022-03-01 10:28:40 -0600

    edit, revisions

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraper
+import scraping
 
 app = Flask(__name__)
 
@@ -16,7 +16,7 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars # reference the database using a variable
-   mars_data = scraper.scrape_all() # 
+   mars_data = scraping.scrape_all() # 
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
